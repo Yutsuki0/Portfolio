72130c602970e56980186d512b74d079659f544a
Yutsuki0 <150014631+Yutsuki0@users.noreply.github.com>
2024-11-20 19:59:22 +0100

    Add files via upload

--- assets/js/nike.js
+++ assets/js/nike.js
@@ -8,3 +8,40 @@
         }
     };
 
+    const imageScroll = document.querySelector('.image-scroll');
+
+    let isMouseDown = false;
+    let startX, scrollLeft;
+    
+    imageScroll.addEventListener('mousedown', (e) => {
+        isMouseDown = true;
+        startX = e.pageX - imageScroll.offsetLeft;
+        scrollLeft = imageScroll.scrollLeft;
+    });
+    
+    imageScroll.addEventListener('mouseleave', () => {
+        isMouseDown = false;
+    });
+    
+    imageScroll.addEventListener('mouseup', () => {
+        isMouseDown = false;
+    });
+    
+    imageScroll.addEventListener('mousemove', (e) => {
+        if (!isMouseDown) return;
+        e.preventDefault();  // Empêche la sélection du texte ou tout autre comportement
+        const x = e.pageX - imageScroll.offsetLeft;
+        const walk = (x - startX) * 2;  // Multiplication par 2 pour augmenter la vitesse de défilement
+        imageScroll.scrollLeft = scrollLeft - walk;
+    });
+    
+
+    const imageScroll2 = document.querySelector('.image-scroll');
+const progress = document.querySelector('.progress');
+const totalScrollWidth = imageScroll.scrollWidth - imageScroll.clientWidth;
+
+imageScroll.addEventListener('scroll', () => {
+    // Calculer la progression du scroll en pourcentage
+    const scrollPercentage = (imageScroll.scrollLeft / totalScrollWidth) * 100;
+    progress.style.width = scrollPercentage + '%'; // Ajuster la largeur de la barre de progression
+});
